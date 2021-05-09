9ce11b3b583895c931dce7f4ef0a02b7a0747eed
avish <avish@Avishek>
2021-05-09 21:27:34 +0530

    Swagger annotations

--- ab-spring-boot-employee-service/src/main/java/com/ab/restfulws/controller/EmployeeController.java
+++ ab-spring-boot-employee-service/src/main/java/com/ab/restfulws/controller/EmployeeController.java
@@ -14,6 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ab.restfulws.model.Employee;
 import com.ab.restfulws.service.EmployeeService;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 @RestController
 
 public class EmployeeController {
@@ -21,38 +26,50 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService employeeService;
 
+	@ApiOperation(value = "getAllEmployee", notes = "Fetch all employee details", nickname = "getAllEmployee")
+	@ApiResponses(value = { @ApiResponse(code = 500, message = "Server error"),
+			@ApiResponse(code = 404, message = "Service not found"),
+			@ApiResponse(code = 200, message = "Successful retrieval", response = Employee.class, responseContainer = "List") })
+
 	@GetMapping("/employee/all")
 	public List<Employee> getAllEmployees() {
 		return employeeService.getAllEmployees();
 
 	}
 
+	@ApiOperation(value = "getEmployeeById", notes = "Fetch  employee details by Id", nickname = "getEmployeeById")
 	@GetMapping("/employee/id/{employeeId}")
-	public Employee getEmployeeById(@PathVariable int employeeId) {
+	public Employee getEmployeeById(
+			@ApiParam(value = "employeeId", required = true, defaultValue = "1") @PathVariable int employeeId) {
 		return employeeService.getEmployeeByEmployeeId(employeeId);
 
 	}
 
+	@ApiOperation(value = "saveEmployee", notes = "Save  employee details", nickname = "saveEmployee")
 	@PostMapping("/employee/save")
 	public Employee saveEmployee(@RequestBody Employee employee) {
 		return employeeService.saveEmployee(employee);
 
 	}
 
+	@ApiOperation(value = "saveEmployees", notes = "Save List of employee details", nickname = "saveEmployees")
 	@PostMapping("/employee/saveall")
 	public List<Employee> saveEmployees(@RequestBody List<Employee> employees) {
 		return employeeService.saveEmployees(employees);
 
 	}
 
+	@ApiOperation(value = "updateEmployee", notes = "Update employee details", nickname = "updateEmployee")
 	@PutMapping("/employee/update")
 	public Employee updateEmployee(@RequestBody Employee employee) {
 		return employeeService.updateEmployee(employee);
 
 	}
 
+	@ApiOperation(value = "deleteEmployeeByEmployeeId", notes = "Delete employee details by Id", nickname = "deleteEmployeeByEmployeeId")
 	@DeleteMapping("/employee/id/{employeeId}")
-	public String deleteEmployeeByEmployeeId(@PathVariable int employeeId) {
+	public String deleteEmployeeByEmployeeId(
+			@ApiParam(value = "employeeId", required = true, defaultValue = "1") @PathVariable int employeeId) {
 		return employeeService.deleteEmployeeByEmployeeId(employeeId);
 
 	}
--- ab-spring-boot-employee-service/src/main/java/com/ab/restfulws/model/Employee.java
+++ ab-spring-boot-employee-service/src/main/java/com/ab/restfulws/model/Employee.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,14 +15,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@ApiModel(description = "Employee Model")
 public class Employee {
 	@Id
 	@GeneratedValue
+	@ApiModelProperty(position = 1, value = "1")
 	private int employeeID;
 	@Column
+	@ApiModelProperty(position = 2, value = "Avishek")
 	private String employeeName;
-	
 
-
-	
 }
